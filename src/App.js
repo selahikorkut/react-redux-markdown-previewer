ce469b6a5217ed2be574170f77e7466c17cb1264
Selahi KORKUT <37619156+selahikorkut@users.noreply.github.com>
2022-05-11 11:35:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Help from './components/Help/Help';
+import Input from './components/Input/Input';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+    <Provider store={store}>
+    <Help/>
+    <Header/>
+    <Input/>
+    </Provider>
+    </div>
+  );
+}
+
+export default App;
